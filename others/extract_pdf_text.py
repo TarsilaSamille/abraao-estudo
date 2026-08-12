6864cd72eb10b37bde2ef1aa49acc3c77be16222
Társila Samille <tarsillasamile@gmail.com>
2026-08-12 14:53:25 -0300

    .

--- others/extract_pdf_text.py
+++
@@ -1,5 +0,0 @@
-from pdf2image import convert_from_path
-
-pages = convert_from_path("abraham-teacher-notes.pdf")
-for idx, page in enumerate(pages):
-    page.save(f"pdf-images/page_{idx}.png", "PNG")
